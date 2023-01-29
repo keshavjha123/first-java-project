1fa05e03cb1b87899ac157881d860cc020495019
KeshavASUS <keshavappujha@gmail.com>
2023-01-29 23:50:58 +0530

    first commit

---
+++ src/main/java/com/javaproject/firstjavaproject/FirstJavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.javaproject.firstjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaProjectApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/javaproject/firstjavaproject/FirstJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.firstjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
